7bfff7113f6b38cd40e6e7653e800dd3ba39e0b3
shashinanupa <shashinanupa365@gmail.com>
2026-04-28 13:23:00 +0530

    update👌

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import itemRoutes from "./routes/itemRoutes.js";
-import dns from 'dns';
-dns.setDefaultResultOrder('ipv4first');
+
 dotenv.config();
 
 const app = express();
@@ -20,10 +19,12 @@ app.use("/api/items", itemRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+// MongoDB connect + server start
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
+
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
